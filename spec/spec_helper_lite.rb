7049ff05a46e511418faed418d87585f00d229b7
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-03-20 21:11:58 +0200

    Use coverage fiter

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -1,11 +1,4 @@
-if !!ENV['CI']
-  require 'coveralls'
-  Coveralls.wear!
-else
-  require 'pry'
-end
-
-ENV['RAILS_ENV'] ||= 'test'
 require 'rspec'
+require_relative "support/ci_helper"
 
 $: << File.expand_path('../lib', File.dirname(__FILE__))
