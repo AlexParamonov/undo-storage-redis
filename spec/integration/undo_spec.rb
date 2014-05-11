ea4239871379eee95e4fe5aea34193ada671a07f
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-05-11 20:31:06 +0300

    Update Undo

--- spec/integration/undo_spec.rb
+++ spec/integration/undo_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper_lite"
 require "undo"
 require "redis"
-require_relative '../../../undo/integration/shared_undo_integration_examples.rb'
+require 'undo/integration/shared_undo_integration_examples.rb'
 
 Undo.configure do |config|
   config.storage = Undo::Storage::Redis.new Redis.new
